eda1e7f03142c62f71e3c526925db4eeceddd41b
stevolopolis <stevenluots@gmail.com>
2024-04-20 02:30:54 +0800

    added multi seed training

--- exp_scale_up.py
+++ exp_scale_up.py
@@ -24,51 +24,54 @@ n_trials = 5
 n_samples = 50000
 n = 1000
 epoch = 25000
+device = "cuda:0"
 
 # Animation parameters
 nframes = 30
 
-def train():
+def train(trial, n_seeds):
     # Data setup
-    sample = torch.tensor(np.linspace(0, 1, n_samples)).to(torch.float32).to("cuda")
+    sample = torch.tensor(np.linspace(0, 1, n_samples)).to(torch.float32).to(device)
 
     n_max_pieces = 1000
     n_segs = 2
 
-    for trial in range(n_trials):
-        analytical_save_path = f"vis/{experiment_name}/analytical/{trial}"
-        empirical_save_path = f"vis/{experiment_name}/empirical/{trial}"
-        create_subdirectories(analytical_save_path)
-        create_subdirectories(empirical_save_path)
+    analytical_save_path = f"vis/{experiment_name}/analytical/{trial}"
+    empirical_save_path = f"vis/{experiment_name}/empirical/{trial}"
+    create_subdirectories(analytical_save_path)
+    create_subdirectories(empirical_save_path)
 
-        # Generate signal
-        segmented_n_pieces = [random.randint(1, n_max_pieces) for _ in range(n_segs)]
-        signal, _, _, _ = generate_piecewise_signal(sample, segmented_n_pieces, seed=trial)
+    # Generate signal
+    segmented_n_pieces = [random.randint(1, n_max_pieces) for _ in range(n_segs)]
+    signal, _, _, _ = generate_piecewise_signal(sample, segmented_n_pieces, seed=trial)
 
-        # Save data & configs
-        save_data(sample.cpu().numpy(), signal.cpu().numpy(), f"{empirical_save_path}/data.npy")
-        save_vals(segmented_n_pieces, f"{empirical_save_path}/n_pieces.txt")
-
+    # Save data & configs
+    save_data(sample.cpu().numpy(), signal.cpu().numpy(), f"{empirical_save_path}/data.npy")
+    save_vals(segmented_n_pieces, f"{empirical_save_path}/n_pieces.txt")
+    
+    model_losses = []
+    for seed in range(n_seeds):
         # Generate specific hash vals
         mid_hashes = [round(0.1 * i, 1) for i in range(1, 10)]
         min_hash, max_hash = 0.0, 1.0
         for mid_hash in mid_hashes:
-            new_hash_vals1 = torch.linspace(min_hash, mid_hash, n_samples // 2).unsqueeze(1).to("cuda")
-            new_hash_vals2 = torch.linspace(mid_hash, max_hash, n_samples // 2).unsqueeze(1).to("cuda")
+            new_hash_vals1 = torch.linspace(min_hash, mid_hash, n_samples // 2).unsqueeze(1).to(device)
+            new_hash_vals2 = torch.linspace(mid_hash, max_hash, n_samples // 2).unsqueeze(1).to(device)
             new_hash_vals = torch.cat((new_hash_vals1, new_hash_vals2), dim=0)
 
             # Model training
             model, configs, model_loss, model_preds = trainer(MODEL, sample, signal, epoch, nframes, hash_vals=new_hash_vals)
-            
+            model_losses.append(model_loss)
             # Animate model predictions
-            animate_model_preds(sample, signal, model_preds, nframes, f"{empirical_save_path}/preds_{mid_hash}.mp4")
-            # Save model configs
-            save_configs(configs, f"{empirical_save_path}/configs.json")
-            # Save model loss
-            save_vals([model_loss], f"{empirical_save_path}/loss_{mid_hash}.txt")
+            animate_model_preds(sample, signal, model_preds, nframes, f"{empirical_save_path}/preds_{mid_hash}_{seed}.mp4")
             # Save model weights
-            torch.save(model.state_dict(), f"{empirical_save_path}/weights_{mid_hash}.pth")
-            print(f"model weights saved at {empirical_save_path}/weights_{mid_hash}.pth")
+            torch.save(model.state_dict(), f"{empirical_save_path}/weights_{mid_hash}_{seed}.pth")
+            print(f"model weights saved at {empirical_save_path}/weights_{mid_hash}_{seed}.pth")
+
+    # Save model configs
+    save_configs(configs, f"{empirical_save_path}/configs.json")
+    # Save model loss
+    save_vals(model_losses, f"{empirical_save_path}/loss_{mid_hash}.txt")
 
 
 def plot(trial):
@@ -98,5 +101,5 @@ def plot(trial):
     plt.close()
 
 if __name__ == "__main__":
-    # train()
-    plot(1)
+    train(10, 3)
+    # plot(1)
