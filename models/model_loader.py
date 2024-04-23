a1e1a3d70b28adf424e1a1d3796366236b5c1b35
steven@paperspace <stevenluots@gmail.com>
2024-04-23 16:02:22 +0000

    minor fixes and model loader

--- models/model_loader.py
+++ models/model_loader.py
@@ -30,7 +30,17 @@ def get_default_model_configs(model_type):
     elif model_type == "ngp_2d":
         Config = namedtuple("config", ["NET"])
         NetworkConfig = namedtuple("NET", ["dim_hidden", "n_levels", "feature_dim", "log2_n_features", "base_resolution", "finest_resolution", "num_layers"])
-        c_net = NetworkConfig(dim_hidden=hidden_dim, n_levels=1, feature_dim=2, log2_n_features=14, base_resolution=50, finest_resolution=50, num_layers=n_layers)
+        c_net = NetworkConfig(dim_hidden=256, n_levels=1, feature_dim=2, log2_n_features=14, base_resolution=50, finest_resolution=50, num_layers=n_layers)
+        c = Config(NET=c_net)
+    elif model_type == "ngp_2d_one2one":
+        Config = namedtuple("config", ["NET"])
+        NetworkConfig = namedtuple("NET", ["dim_hidden", "n_levels", "feature_dim", "log2_n_features", "base_resolution", "finest_resolution", "num_layers"])
+        c_net = NetworkConfig(dim_hidden=64, n_levels=1, feature_dim=2, log2_n_features=18, base_resolution=50, finest_resolution=50, num_layers=n_layers)
+        c = Config(NET=c_net)
+    elif model_type == "ngp_multilevel_2d":
+        Config = namedtuple("config", ["NET"])
+        NetworkConfig = namedtuple("NET", ["dim_hidden", "n_levels", "feature_dim", "log2_n_features", "base_resolution", "finest_resolution", "num_layers"])
+        c_net = NetworkConfig(dim_hidden=64, n_levels=8, feature_dim=2, log2_n_features=12, base_resolution=16, finest_resolution=256, num_layers=3)
         c = Config(NET=c_net)
     else:
         raise ValueError("Model type not recognized")
@@ -49,7 +59,10 @@ def get_default_model_opts(model_type, model, epoch=5000):
     elif model_type == "ngp":
         optim = torch.optim.Adam(model.parameters(), lr=0.01)
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, epoch, eta_min=1e-4)
-    elif model_type == "ngp_2d":
+    elif model_type == "ngp_2d" or model_type == "ngp_2d_one2one":
+        optim = torch.optim.Adam(model.parameters(), lr=0.01)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, epoch, eta_min=1e-4)
+    elif model_type == "ngp_multilevel_2d":
         optim = torch.optim.Adam(model.parameters(), lr=0.01)
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, epoch, eta_min=1e-4)
     else:
@@ -63,7 +76,7 @@ def get_model(model_type, dim_in, dim_out, data_size, configs, device="cuda"):
         model = MLP(dim_in, dim_out, hidden_dim, configs).to(device)
     elif model_type == 'linear':
         model = LinearModel().to(device)
-    elif model_type in ["ngp", "ngp_2d"]:
+    elif model_type in ["ngp", "ngp_2d", "ngp_multilevel_2d", "ngp_2d_one2one"]:
         model = NGP(dim_in, dim_out, data_size, configs).to(device)
     else:
         raise ValueError("Model type not recognized")
